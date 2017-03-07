f7a0c93ffecfc16eab58bc7ae81ccf7792003b25
arya <airanmehr@gmail.com>
2017-03-07 02:14:29 -0800

    compute AF

--- Scripts/computeAF.sh
+++ Scripts/computeAF.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
-PYTHONPATH="${PWD%/*}"
-echo $PYTHONPATH
-for i in {1..22} X Y
-do
-    python ./computeAF.py $i
-done
+#!/bin/bash
+
+#PBS -m ae
+
+#PBS -l walltime=200:00:00
+
+#PBS -V
+#PBS -e ~/qsub.out
+#PBS -o ~/qsub.out
+#PBS -l mem=30gb
+#PBS -j oe
+#PBS -p 100
+cd "$PBS_O_WORKDIR"
+echo $CHROM >>~/qsub.out
+python ./python.py $CHROM  >> ~/qsub.out 2>> ~/qsub.out
