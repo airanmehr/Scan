5b0cb8415ad2bbe5542002a22fb771dfca75df66
arya <airanmehr@gmail.com>
2017-03-26 12:50:22 -0700

    sync moved

--- Scripts/shell/sync.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-rsync -arzuv  genomequery.ucsd.edu:/home/arya/storage/Data/Human/ /home/arya/storage/Data/Human/ #--delete
-rsync -arzuv  /home/arya/storage/Data/Human/ genomequery.ucsd.edu:/home/arya/storage/Data/Human/ #--delete
